70fc47ad7422c1cdeb7b7ac487b3ea762e45d04f
Kirill <89221688844@mail.ru>
2023-05-22 21:37:44 +0500

    Производительность 08

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ export const styles = () => {
 const html = () => {
   return gulp.src('source/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('build'));
 }
 
 // Scripts
@@ -42,7 +42,7 @@ const html = () => {
 const scripts = () => {
   return gulp.src('source/js/*.js')
     .pipe(terser())
-    .pipe(gulp.dest('build/js'))
+    .pipe(gulp.dest('build/js'));
 }
 
 // Images
@@ -128,7 +128,7 @@ const reload = (done) => {
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/script.js', gulp.series(scripts));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 // Build
